49f7c6c062f2ecdbf3480aab45fe82491efa896e
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2026-03-04 17:09:08 +0200

    Change the base url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,10 @@
 // lib/config.ts
 
-const isProduction = process.env.NODE_ENV === "production";
+const isVercelPreview =
+  typeof window !== "undefined" &&
+  window.location.origin === "https://barq-admin.vercel.app";
+
+const isProduction = process.env.NODE_ENV === "production" && !isVercelPreview;
 
 export const BASE_URL = isProduction
   ? "https://api.barqshipping.com/api/v1"
